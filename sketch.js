907f37eabbd4069b1f20e6946ab2c1e576731266
Catalina Restrepo Salgado <rposalgadoc@gmail.com>
2023-06-13 22:41:22 -0500

    Add images and blur filter

--- sketch.js
+++ sketch.js
@@ -12,10 +12,12 @@ let myShader;
 let amplitud;
 let fft;
 let isListening = false;
-let color = 155;
 let noise;
 let isShowing = false;
 let soundPlayed = true;
+let energy;
+let background_1;
+let background_2;
 
 function preload() {
     
@@ -37,22 +39,23 @@ function preload() {
     correct_ans = loadSound("sound_files/correctsound");
 
     myShader = loadShader("shaders/shader.vert", "shaders/shader.frag");
-
-    noise = loadImage("noise.png");
+    background_1 = loadImage("images/background_1.png");
+    background_2 = loadImage("images/background_2.png");
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
 
     angleMode(DEGREES);
+    imageMode(CENTER);
 
-    //let marginPercentage = 5;
-    //let marginX = (windowWidth * marginPercentage) / 100;
-    //let marginY = (windowHeight * marginPercentage) / 100;
+    amplitud = new p5.Amplitude();
+    fft = new p5.FFT();
+
+    background_1.filter(BLUR, 12);
+    background_2.filter(BLUR, 12);
 
     puntosParrafo = createP(puntos + "/12"); 
-    //puntosParrafo.position(windowWidth - (windowWidth - 1500), windowHeight - (windowHeight-60)); 
-    //puntosParrafo.position(marginX, marginY);
     puntosParrafo.style('position', 'fixed');
     puntosParrafo.style('top', '-5%');
     puntosParrafo.style('right', '5%');
@@ -60,26 +63,23 @@ function setup() {
     puntosParrafo.style("font-size", "60px"); 
     puntosParrafo.style("font-family", "Verdana");
     puntosParrafo.style("font-weight", "700");
-    //puntosParrafo.style("font-family", "papyrus");
     puntosParrafo.style("color", "white"); 
     puntosParrafo.style("padding", "10px"); 
     puntosParrafo.style("border-radius", "5px"); 
-    background(color);
 
+    background(0);
+    
     speechRec = new p5.SpeechRec('es-ES');
     speechRec.continuous = true; // Habilitar reconocimiento de voz continuo
     speechRec.interimResults = false; // Habilitar resultados intermedios
     speechRec.onResult = gotSpeech;
-    //speechRec.start();
-
-    amplitud = new p5.Amplitude();
-    fft = new p5.FFT();
-
+  
     // Crear botón para activar y desactivar el reconocimiento de voz
-    let activateButton = createButton("Quiero hablar");
+    activateButton = createButton("No te escucho");
     activateButton.position(windowWidth - (windowWidth - 50), windowHeight - (windowHeight-50));
     activateButton.mousePressed(activateRecognition);
-    //Estilización del botón
+
+    // Estilización del botón
     activateButton.style("background-image", "linear-gradient(135deg, #f34079 40%, #fc894d)");
     activateButton.style("border", 0);
     activateButton.style("align-items", "center");
@@ -97,15 +97,15 @@ function setup() {
     activateButton.style("width", "200px")
     activateButton.style("justify-content", "center");
     activateButton.style("text-transform", "uppercase");
+
+    
 }
 
 function activateRecognition() {
   if (!isListening) {
-    color = 0;
     speechRec.start(); // Iniciar el reconocimiento de voz
     isListening = true;
   } else {
-    color = 155;
     speechRec.stop(); // Detener el reconocimiento de voz
     isListening = false;
   }
@@ -122,16 +122,22 @@ function gotSpeech() {
 puntos = 0; 
 function draw() {
 
-  background(color);
+  background(0);
+
+  fft.analyze();
+  energy = fft.getEnergy(20, 200);
+
+  if (!isListening) {
+    activateButton.html("No te escucho");
+  } else {
+    activateButton.html("Te escucho");
+  }
 
   shader(myShader);
   myShader.setUniform("uFrameCount", frameCount);
 
   let wave = fft.waveform();
   let level = amplitud.getLevel();
-
-  // Pasamos la amplitud al shader
-  // myShader.setUniform("uLevel", level);
 
   // Condiciones para reproducir y detener canciones
   if (speechValue == "para" && isPlaying){
@@ -140,17 +146,9 @@ function draw() {
   } else if (speechValue == "siguiente" && !isPlaying) {
     soundPlayed = false;
     empezar();
-    // Send the frameCount to the shader
-    // myShader.setUniform("uFrameCount", frameCount);
-    // myShader.setUniform("uNoiseTexture", noise);
 
-    // Rotate our geometry on the X and Y axes
-    // rotateX(frameCount * 0.01);
-    // rotateY(frameCount * 0.005);
-  } 
-
-  else if (speechValue == canciones[auxiliar][1]) {
-    console.log("Correcto");
+  // Reconocimiento del título de la canción
+  } else if (speechValue == canciones[auxiliar][1]) {
     puntos = puntos + 1;  
     parar();
     correct_ans.play();
@@ -158,16 +156,30 @@ function draw() {
     puntosParrafo.html(puntos + "/12");
     console.log(puntos);
     soundPlayed = false;
+  } else if (auxiliar == 0 && isPlaying) {
+    if (speechValue != canciones[auxiliar][1] && speechValue != "incorrecto" && speechValue != "para" && speechValue != "siguiente" && speechValue != undefined && speechValue.length != 0) {
+      if (!soundPlayed) {
+        wrong_ans.play();
+        soundPlayed = true; // Establecer la variable de estado a true para indicar que el sonido se reprodujo
+      }
+    }
+  } else if (auxiliar != 0 && isPlaying) {
+    if (speechValue != canciones[auxiliar][1] && speechValue != canciones[auxiliar-1][1] && speechValue != "incorrecto" && speechValue != "para" && speechValue != "siguiente" && speechValue != undefined && speechValue.length != 0) {
+      if (!soundPlayed) {
+        wrong_ans.play();
+        soundPlayed = true; // Establecer la variable de estado a true para indicar que el sonido se reprodujo
+      }
+    }
+  } 
+
+  // Texto auxiliar para una respuesta incorrecta
+  if (soundPlayed) {
+    speechValue = "incorrecto";
   }
 
-  else if (speechValue != canciones[auxiliar][1] && speechValue != undefined && speechValue != "para" && speechValue != "siguiente" && speechValue == [] && speechValue == ""){
-    console.log("incorrecto");
-    if (!soundPlayed) {
-      wrong_ans.play();
-      soundPlayed = true; // Establecer la variable de estado a true para indicar que el sonido se reprodujo
-      
-    }
-    
+  // Posibilidad de volver a intentar adivinar el nombre de la canción
+  if (speechValue == "incorrecto") {
+    soundPlayed = false;
   }
 
   // Cambio de índice para comenzar nuevamente
@@ -181,50 +193,43 @@ function draw() {
     puntosParrafo.html(puntos + "/12");
   }
 
-  // Reconocimiento del título de la canción
-
-
-
-  /*if (index % 2 == 0){
-    firstVisualizer(level);
-  } else {
-    secondVisualizer(wave);
-    // shader() sets the active shader with our shader
-    //shader(myShader);
-      // Send the frameCount to the shader
-    //myShader.setUniform("uFrameCount", frameCount);
-
-    // Rotamos la figura en función del tiempo y la amplitud del audio
-    //rotateX(frameCount * 0.01 + level);
-    //rotateY(frameCount * 0.005 + level);
-
-    // Draw some geometry to the screen
-  // We're going to tessellate the sphere a bit so we have some more geometry to work with
-    //sphere(width / 7, 200, 200);
-  } */
-
   if (isShowing) {
+    push();
+    if (energy > 200) {
+      rotate(random(-0.5, 0.5));
+    }
+    image(background_2, 0, 0, width + 100, height + 100);
+    pop();
+
     fill(255);    
     
     rotateX(frameCount * level);
     rotateY(frameCount * level);
     sphere(width / 7, 200, 200);
+
   } else {
+    push();
+    if (energy > 200) {
+      rotate(random(-0.5, 0.5));
+    }
+    image(background_1, 0, 0, width + 100, height + 100);
+    pop();
+
     stroke(255);
-    strokeWeight(3);
+    strokeWeight(5);
     noFill();
   
     for (let t = -1; t <= 1; t += 2) {
       beginShape();
       for (let i = 0; i <= 180; i += 1) {
         let index = floor(map(i, 0, 180, 0, wave.length - 1));
-        let r = map(wave[index], -1, 1, 150, 350);
-        vertex(r * sin(i) * t, r * cos(i)* t);
+        let amplitude = map(wave[index], -1, 1, 0.2, 1.0); // Ajuste del mapeo de amplitud
+        let r = map(amplitude, 0.2, 1.0, 150, 350); // Ajuste del mapeo de radio
+        vertex(r * sin(i) * t, r * cos(i) * t);
       }
       endShape();
     }
   }
-  
 }
 
 function parar() {
